b3d03b1e3e851f84834cf3ce83ba0f2476aeb7a3
geordyp <geowilliams6@gmail.com>
2017-09-14 16:03:54 -0500

    Set up database

--- pg_config.sh
+++ pg_config.sh
@@ -1,4 +1,2 @@
 apt-get -qqy update
-apt-get -qqy install postgresql python-psycopg2
-apt-get -qqy install python-sqlalchemy
-apt-get -qqy install python-pip
+apt-get -qqy install python-pip python-dev postgresql
