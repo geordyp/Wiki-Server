8cd8ec185d6a5bb36c428876106245baee9be216
geordyp <geowilliams6@gmail.com>
2017-10-26 23:17:06 +0000

    Edit database

--- researchproject/wikiserver/admin.py
+++ researchproject/wikiserver/admin.py
@@ -3,10 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import User, Post, Page
+from .models import User, Page
 
 # Register your models here.
 
 admin.site.register(User)
-admin.site.register(Post)
 admin.site.register(Page)
--- researchproject/wikiserver/models.py
+++ researchproject/wikiserver/models.py
@@ -6,27 +6,19 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    fullname = models.CharField(max_length=200, null=False, blank=False)
+    username = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    email = models.CharField(max_length=200, null=False, blank=False, unique=True)
 
     def __str__(self):
         return self.name;
 
 
 class Page(models.Model):
-    title = models.CharField(max_length=300)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.title;
-
-
-class Post(models.Model):
-    title = models.CharField(max_length=300)
-    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300, null=False, blank=False, unique=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
     content = models.TextField(max_length=1000)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.title;
