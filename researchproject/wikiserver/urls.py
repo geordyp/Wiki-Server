23bb09e33da41cc0e6d5017b6df0fe0d2fc03a8b
geordyp <geowilliams6@gmail.com>
2017-10-26 23:19:16 +0000

    Add view

--- researchproject/wikiserver/urls.py
+++ researchproject/wikiserver/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 
 urlpatterns = [
+    # ex: /wiki/
     url(r'^$', views.index, name='index'),
+    # ex: /wiki/5/
+    url(r'^(?P<page_id>[0-9]+)/$', views.page, name='page')
 ]
--- researchproject/wikiserver/views.py
+++ researchproject/wikiserver/views.py
@@ -7,3 +7,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the wiki index.")
+
+
+def page(request, page_id):
+    return HttpResponse("You're looking at page %s." % page_id)
