cb08ea0ffe9db026a4505e624f62c90b983ea77f
geordyp <geowilliams6@gmail.com>
2018-01-24 11:59:31 -0600

    Add tests for user account creation

--- researchproject/wikiserver/tests.py
+++ researchproject/wikiserver/tests.py
@@ -21,6 +21,11 @@ class UserAccountTests(TestCase):
         response = self.client.post(reverse('wikiserver:createuseraccount'),data={'username':'uniquename', 'password':''})
         self.assertEqual(response.status_code, 400)
 
+        # short password
+        response = self.client.post(reverse('wikiserver:createuseraccount'),data={'username':'uniquename', 'password':'aaa'})
+        self.assertEqual(response.status_code, 400)
+
+
     def test_create_user_account_username_invalid(self):
         """
         test invalid form input for username
@@ -32,4 +37,17 @@ class UserAccountTests(TestCase):
 
         # blank username
         response = self.client.post(reverse('wikiserver:createuseraccount'),data={'username':'', 'password':'insecurepassword'})
+        self.assertEqual(response.status_code, 400)
+
+        # non-alpha-numeric username
+        response = self.client.post(reverse('wikiserver:createuseraccount'),data={'username':'yo!', 'password':'insecurepassword'})
+        self.assertEqual(response.status_code, 400)
+
+        # non-alpha-numeric username
+        response = self.client.post(reverse('wikiserver:createuseraccount'),data={'username':'johnny-apple-seed', 'password':'insecurepassword'})
+        self.assertEqual(response.status_code, 400)
+
+        # taken username
+        self.client.post(reverse('wikiserver:createuseraccount'),data={'username':'john', 'password':'insecurepassword'})
+        response = self.client.post(reverse('wikiserver:createuseraccount'),data={'username':'john', 'password':'insecurepassword'})
         self.assertEqual(response.status_code, 400)
