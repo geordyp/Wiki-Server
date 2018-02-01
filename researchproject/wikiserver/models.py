b86531512663a39e2b8813155fe1976df3471d94
geordyp <geowilliams6@gmail.com>
2018-02-01 11:02:27 -0600

    Change Post model configuration
    Remove unique constraint for post title and increase the max length of
    the content.

--- researchproject/wikiserver/models.py
+++ researchproject/wikiserver/models.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=300, null=False, blank=False, unique=True)
+    title = models.CharField(max_length=300, null=False, blank=False)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
-    content = models.TextField(max_length=1000)
+    content = models.TextField(max_length=40000)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
 
     def __str__(self):
