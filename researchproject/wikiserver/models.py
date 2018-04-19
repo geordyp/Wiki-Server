ac1fcc2cdaa9b550cec22398e2ffe8118b372b32
geordyp <geowilliams6@gmail.com>
2018-04-19 16:36:50 -0500

    Update model

--- researchproject/wikiserver/models.py
+++ researchproject/wikiserver/models.py
@@ -6,10 +6,10 @@ from django.contrib.auth.models import User
 
 
 class Page(models.Model):
-    title = models.CharField(max_length=300, null=True, blank=False)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=False)
-    content = models.TextField(max_length=40000, default='no content', null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=300, default='title unavailable', null=False, blank=True)
+    content = models.TextField(max_length=40000, default='content unavailable', null=False, blank=True)
     pub_date = models.DateTimeField('date published', auto_now_add=True, null=True)
 
     def __str__(self):
-        return self.title;
+        return "%s BY %s" % self.title, self.owner.username;
